360f90640313e00bcaea7203c42919924ec0c25a
Allison Snipes <ajsnipes@usfca.edu>
2020-06-07 09:32:24 -0700

    first commit

---
+++ BmiProgram.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class BmiProgram {
+
+	public static void main(String[] args) {
+		System.out.println("hello world.");
+		// using the scanner input method we need to gather the users' input
+		Scanner input = new Scanner(System.in);
+	}
+
+}
