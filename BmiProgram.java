edf501467b63f1b21b4e7838d67a2dde354fee31
Allison Snipes <ajsnipes@usfca.edu>
2020-06-07 09:34:39 -0700

    upating bmi code

--- BmiProgram.java
+++ BmiProgram.java
@@ -7,5 +7,17 @@ public class BmiProgram {
 		// using the scanner input method we need to gather the users' input
 		Scanner input = new Scanner(System.in);
 	}
+	
+	/** Here I would like to display a greeting to the user to explain to them the purpose of the application.
+		This will require me to make a function called HeaderMessage. I delegated the function as private since
+		I do not want other aspects of my program to bother what I have written inside of the HeaderMessage function.
+		I learned about the difference between private and public functions prior to the start of the summer 2020 term 
+		via reading the textbook, and following along with online tutorials. (Java for Students--UK:
+		https://www.javaforstudents.co.uk/Practice/Bank_account)
+	*/
+	private static void HeaderMessage() {
+		System.out.println()
+	}
+	
 
 }
